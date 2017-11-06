bbf69819d9facd855c701c3f158d34a0f03ba327
Binita Mehta <binita_mehta@yahoo.com>
2017-11-06 10:56:52 -0500

    Consolidated the methods to set response headers for CORS.

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var restify = require('restify'),
     numbers = require('./numbers'),
     port = process.env.PORT || 3000;
 
-
 var server = restify.createServer({
     name: 'Numbers server',
     version: '1.0.0'
@@ -16,20 +15,17 @@ server.use(function(req, res, next){
     console.log(req.method + ' ' + req.url);
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token');
+    //res.header("Access-Control-Allow-Headers", "X-Requested-With");
     return next();
 });
 
-server.opts('/\.*/', corsHandler, optionsRoute);
-function corsHandler(req, res, next) {
+server.opts('/\.*/', optionsRoute);
 
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token');
-    res.setHeader('Access-Control-Allow-Methods', '*');
-    res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
-    res.setHeader('Access-Control-Max-Age', '1000');
-
-    return next();
+function setCorsHeaders(res){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
 }
 
 function optionsRoute(req, res, next) {
